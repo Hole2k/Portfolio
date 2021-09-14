9241012397bd044dcbcadfb6c1a275cae524d6cb
Hole2k <malyholek@gmail.com>
2021-09-14 15:46:38 +0200

    My portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/picture.png" alt=""></img>
+                    <img src="assets/man.png" alt=""></img>
                 </div>
             </div>
             <div className="right">
